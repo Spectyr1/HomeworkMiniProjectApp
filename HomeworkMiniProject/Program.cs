d8b2f7b3324a06f940c6b0f3c4f28bdde838e3f7
Danny Warren <danny@theforbiddenarts.com>
2022-03-23 19:04:09 -0400

    submit

--- HomeworkMiniProject/Program.cs
+++ HomeworkMiniProject/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string? firstName;
+int? age;
+
+Console.Write("Hello, please enter your name: ");
+firstName = Console.ReadLine();
+
+Console.Write("Thank you. Next, please enter your age: ");
+age = Int32.Parse(Console.ReadLine());
+
+
+if (firstName.ToLower() == "bob" || firstName.ToLower() == "sue")
+{
+    Console.WriteLine("Hello professor, welcome to my application.");
+}
+else
+{
+    Console.WriteLine($"Hello {firstName}, Welcome to my application.");
+}
+
+if (age < 21)
+{
+    Console.WriteLine($"It is recommended that you wait {21 - age} year(s) to start this class.");
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 HomeworkMiniProjectApp
+
+Plan and build a Console application that asks a user for thier name and age.
+If thier name is Bob or Sue, address them as professor.
+If the person is under 21, recommend they wait X years to start this class.
+
+
+// ask for user name
+// ask for user age
+// Bob and Sue are Professor
+// others are Name
+// under 21 recommend waiting X years til 21
